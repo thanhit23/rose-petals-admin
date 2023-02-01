9c9ab2229f0e57dd66e8f3f87191bf3314b2b38f
Duy Thanh <duythanhit203@gmail.com>
2023-02-01 09:46:02 +0700

    [UI] Redirect to home/dashboard/404 page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+
+import messages from './messages';
+
 function NotFound() {
-  return <h1>Not Found</h1>;
+  return (
+    <div className="relative inset-0 flex h-[100vh] place-content-center place-items-center flex-col">
+      <h1>
+        <FormattedMessage {...messages.title} />
+      </h1>
+      <Link
+        className="flex items-center bg-[#4E97FD] hover:opacity-70 text-white font-semibold hover:text-white py-2 px-4 mt-3 border border-[rgb(78,151,253)] hover:border-transparent rounded"
+        to="/"
+      >
+        <FormattedMessage {...messages.button} />
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
